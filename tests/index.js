5388116513541d60ff1383d9c38412f8ce84158c
itsretr0n <51875905+itsretr0n@users.noreply.github.com>
2019-08-14 21:02:58 -0500

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,7 @@
 //const webcord = require('../main')
 /** END */
 
-const webhookURL = 'https://discordapp.com/api/webhooks/610720483284877338/z0htnwTcfr6Pk6Sf0Y_3LOTuIhcpXEBZcEq6f50q3Ihc7rHikBN-2AG-Ux2RqGdls0cH'
+const webhookURL = 'https://discordapp.com/api/webhooks/611379099390181397/thgTaqen-gK1_h6N-qvOJWEbxK7hPWMNFdd2MaAs0UFDNhadsjjsaasjdsa'
 const avatarURL = 'https://cdn.discordapp.com/avatars/516840368843522073/1040b30414894c8e427ccae7a96d3718.webp?size=128'
 new webcord(webhookURL,'OwO',avatarURL)
 	.setTitle('This works in node and browsers!')
