cb1886e9a490e67ac0b7b032d87dc4d5802a81b0
Hunter Paulson <hpaulson@smc.wtf>
2020-05-07 01:46:12 -0400

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,8 @@
 let webcord = require('../build/index')
 const webhook = 'https://canary.discord.com/api/webhooks/705517758691016765/N8ihaj7G9LAawVTId1Zp_Hyjw9aOAgyojb2q8wpXWeR3ku9GbFzHWskfm9rdXVbmZqCA'
 const avatar = 'https://seismiccore.wtf/assets/logo.png'
-new webcord(webhook, 'Retr0n', avatar)
-.setDescription(`<a:checkv2:699728359805943919> Test`)
+new webcord(webhook, 'Webcord', avatar)
+.setDescription(`Test`)
 .setFooter(new Date())
 .setTimestamp()
 .setColor('0x2F3136')
